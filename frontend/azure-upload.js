5af6d22bb25f3a2f0283cf09e965fdcc81f10cae
Saad <saadaqqh@gmail.com>
2025-04-20 17:29:17 +0500

    Updated the index html to upload file hopefully works now

--- frontend/azure-upload.js
+++ frontend/azure-upload.js
@@ -1,10 +1,8 @@
 const connectionString = "DefaultEndpointsProtocol=https;AccountName=minimindsrg9f5a;AccountKey=cbL49zKjeB/WU9bHibLnYu5A2wKpRd+EDATsZw2YWpwYQiUZeIsQX30cIukGP9CsocTqhb1B133i+AStQDNpPQ==;EndpointSuffix=core.windows.net";
 
-const { BlobServiceClient } = AzureStorageBlob;
-
 async function saveProgressToAzure(userId, progress, score) {
   try {
-    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    const blobServiceClient = Azure.Storage.Blob.BlobServiceClient.fromConnectionString(connectionString);
     const containerClient = blobServiceClient.getContainerClient("user-progress");
 
     const blobName = `${userId}.json`;
