f6d6ad900eb34e3b15989cee172b381b1b9c1283
Saad <saadaqqh@gmail.com>
2025-04-20 12:55:36 +0500

    Frontend v1

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+function login() {
+    const userId = "test_kid123";
+    localStorage.setItem("userId", userId);
+    localStorage.setItem("level", 1);
+    localStorage.setItem("score", 50);
+    window.location.href = "home.html";
+  }
+  
+  window.addEventListener("DOMContentLoaded", () => {
+    const userIdDisplay = document.getElementById("userIdDisplay");
+    const levelDisplay = document.getElementById("levelDisplay");
+    const scoreDisplay = document.getElementById("scoreDisplay");
+  
+    if (userIdDisplay && levelDisplay && scoreDisplay) {
+      const userId = localStorage.getItem("userId") || "Unknown";
+      const level = localStorage.getItem("level") || 0;
+      const score = localStorage.getItem("score") || 0;
+  
+      userIdDisplay.textContent = userId;
+      levelDisplay.textContent = level;
+      scoreDisplay.textContent = score;
+    }
+  });
+  
