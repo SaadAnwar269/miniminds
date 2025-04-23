df8e0daedb61fc3230e0ada88a22d3006621007d
Saad <saadaqqh@gmail.com>
2025-04-23 13:58:40 +0500

    Deleted duplicate files

--- frontend/static/script.js
+++
@@ -1,7 +0,0 @@
-async function login() {
-    const userId = prompt("Enter your user ID (e.g., kid123):");
-    window.location.href = "home.html";
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-});
